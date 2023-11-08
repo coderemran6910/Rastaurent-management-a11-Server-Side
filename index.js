63399b96ec2a1667fd2ac5ecba42156d1c389de1
Coder Emran <coderemran6910@gmail.com>
2023-11-08 18:57:10 +0600

    added jwt feature API

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    // 'http://localhost:5173'
+    'https://rastaurent-management-a-11.web.app/', 
+    'https://rastaurent-management-a-11.firebaseapp.com/'
+],
+  credentials: true
+}));
+app.use(cookieParser());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wmyy1wk.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,6 +39,36 @@ async function run() {
     const allProductsCollection = client.db("foodsDb").collection("food");
     const orderCollection = client.db("foodsDb").collection("order");
     const userCollection = client.db("foodsDb").collection("user");
+
+
+// Token related Api
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1h",
+  })
+
+  res
+ .cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', 
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      
+  })
+  
+  .send({success: true})
+})
+
+app.post('/logout', async(req, res)=>{
+  const user = req.body;
+  console.log('log out', user);
+
+  res.clearCookie('token' , {maxAge:0}).send({success:true})   
+})
+
+
+
+
 
     // Services related Api
     app.post("/api/v1/foods", async (req, res) => {
